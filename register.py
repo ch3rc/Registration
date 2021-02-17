4bd2b836a386a492ad2403c2b1eb1e7d826826f3
Cody <ch3rc@mail.umsl.edu>
2021-02-16 22:17:56 -0600

    well there goes my parser

---
+++ register.py
@@ -0,0 +1,12 @@
+"""
+Author:     Cody Hawkins
+Date:       2/16/21
+Class:      CS6420
+Desc:       Demonstration of manual or automatic
+            registration on an image
+"""
+
+import cv2 as cv
+import numpy as np
+import getopt
+import sys
